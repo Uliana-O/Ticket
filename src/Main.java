e98780946435fb5614c79ccf1934fabeeaf3aeb7
Uliana <uosintseva@gmail.com>
2023-01-30 20:12:16 +0300

    add files

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 6000;
+        int m = 20;
+        int totalMiles = (ticket / m);
+
+        System.out.println("Количество миль:" + (totalMiles));
+    }
+
+}
+
